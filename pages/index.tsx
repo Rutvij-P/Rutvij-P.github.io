7c9aec939635f34b1636c684788af184c9672c91
Rutu Patel <rpatel5@mail.sfsu.edu>
2022-11-02 00:00:27 -0700

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { AiFillGithub, AiFillLinkedin, AiFillMail} from 'react-icons/ai'
 
 export default function Home() {
   return (
@@ -9,13 +11,37 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className=" border-indigo-700 min-h-screen">
-          <h1>Hello</h1>
+      <main className=" bg-gradient-to-tr from-purple-600 to-red-400 bg-orange-400 px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">developedbyRutvij</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="curson-pointer text-2xl"/>
+              </li>
+              <li><a className="bg-gradient-to-r from-yellow-600 to-red-600 bg-amber-500 border-double border-4 border-black text-white px-4 py-2 rounded-md ml-6" href="#">Resume</a></li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 font-medium text-white">Rutvij Patel</h2>
+            <h3 className="text-2xl py-2 text-white">UI/UX Frontend Developer</h3>
+            <p className="text-md py-2 leading-8 text-white">
+            Undergrad student at San Francisco State University
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-32">
+            <AiFillGithub />
+            <AiFillLinkedin />
+            <AiFillMail />
+          </div>
+          <div>
+            {/* for Image here */}
+          </div>
+        </section>
+        <section>
+          
         </section>
       </main>
-
-
     </div>
   )
 }
