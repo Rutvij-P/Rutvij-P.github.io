bc204db6bb05ca4dae2c2c03dcf3bc6c8a5d37d1
Rutu Patel <rpatel5@mail.sfsu.edu>
2022-11-04 02:13:35 -0700

    created new section and styled image

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,9 @@ export default function Home(): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=" bg-gradient-to-tr from-purple-600 to-red-400 bg-orange-400 px-10 ">
-        <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+      <main className=" bg-gradient-to-tr from-purple-600 to-red-400 bg-orange-400 px-10 snap-y overflow-scroll">
+        <section className="min-h-screen snap-start">
+          <nav className="py-10 mb-10 flex justify-between">
             <h1 className="text-xl font-burtons">BuiltbyRutvij</h1>
             <ul className="flex items-center">
               <li>
@@ -27,14 +27,13 @@ export default function Home(): JSX.Element {
           <div className="grid place-content-center">
             <Image
               src="/rutu_patel.jpg"
-              alt="Picture of the author"
-              className="w-85 h-85 mt-10"
+              alt="Your Student Coder"
+              className="w-85 h-85 mt-8 rounded-lg transition-shadow duration-300 hover:shadow-2xl"
               width={350}
               height={350}
-              objectFit='cover'
             />
           </div>
-          <div className="text-center p-10">
+          <div className="text-center p-8">
             <h2 className="text-5xl py-2 font-medium text-white">Rutvij Patel</h2>
             <h3 className="text-2xl py-2 text-white">UI/UX Frontend Developer</h3>
             <p className="text-md py-2 leading-8 text-white">
@@ -49,12 +48,26 @@ export default function Home(): JSX.Element {
             <AiFillMail />
           </div>
         </section>
-        <section className="min-h-screen">
+        <section className="min-h-screen snap-start">
           <h2 className="text-2xl py-2">About</h2>
           <h3 className="text-md py-2 text-white text-justify">Undergrad Computer Science student versed in reviewing plans,
             writing reports, researching solutions, and implementing company
             procedures. Willingness to learn, follow through instructions and
             work cooperatively within team environments.</h3>
+        </section>
+        <section className="min-h-screen snap-start">
+          <h2 className="text-2xl py-2">Projects and Skills</h2>
+          <h3 className="text-md py-2 text-white text-justify">
+            Orion Clock: A basic Java GUI <br />
+            Job-Gram: A responsive Web-App that allows students to connect to recruiters and vice versa. [In P.] <br />
+            Profile Page: A responsive Profile page that serves as a medium for putting my portfolio out there in the world. [In P.] <br />
+          </h3>
+        </section>
+        <section className="min-h-screen snap-start">
+          <h2 className="text-2xl py-2">Contact Me</h2>
+          <h3 className="text-md py-2 text-white text-justify">
+           Contact Me Page Here
+          </h3>
         </section>
       </main>
     </div>
